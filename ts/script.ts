f36e137f7d766620637f6998775d0f15e82b7587
Saori Ito <saori@itoo.info>
2025-02-21 10:48:18 +0900

    簡単なスムーススクロールを実装

--- ts/script.ts
+++ ts/script.ts
@@ -250,11 +250,49 @@ function changeHeaderColorchange() {
   }
 }
 
+function scrollToTarget(targetElement) {
+  window.scrollTo({
+    top: targetElement.offsetTop,
+    behavior: "smooth",
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", function () {
     // スクロールによるヘッダ周りの表示切り替え
     changeLogoVisible();
     changeHeaderColorchange();
+  });
+
+  //  スムーススクロール ////////////////////////////////////////////
+  // ページ読み込み時
+  if (window.location.hash) {
+    const hash = window.location.hash;
+    const targetElement = document.querySelector(hash) as HTMLElement;
+    if (targetElement) {
+      scrollToTarget(targetElement);
+  }
+  // ハッシュが変わった時
+  window.addEventListener("hashchange", (event) => {
+    console.log(window.pageYOffset);
+    const hash = window.location.hash;
+    const targetElement = document.querySelector(hash) as HTMLElement;
+    if (targetElement) {
+     scrollToTarget(targetElement);
+    }
+  });
+  // リンクをクリックした時
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (event) {
+      event.preventDefault();
+      const hash = this.getAttribute("href");
+      const targetElement = document.querySelector(hash) as HTMLElement;
+      if (targetElement) {
+        scrollToTarget(targetElement);
+        // 履歴に追加
+        history.pushState({}, "", hash);
+      }
+    });
   });
 
   getWorksData();
